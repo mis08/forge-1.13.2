99701643a2f450467c2f652cb45444b6eb138f2f
gantz <gantz@DESKTOP-DMLN4TS>
2019-05-05 18:51:58 +0900

    first comit

---
+++ forge1.13.2/src/main/java/com/underground/undergroundmod/item/Itemtest.java
@@ -0,0 +1,15 @@
+package com.underground.undergroundmod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Properties;
+
+public class Itemtest extends Item{
+
+	public Itemtest(Properties properties) {
+		super(properties);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+
+}
+
